41a75f961cffcab610efdd5fa1425693b2ef3276
SpringNuance <xuanbinh.dev@gmail.com>
2021-08-23 15:35:23 +0300

    okay

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let colormode = 0;
+
+function colorMode() {
+    if (colormode === 0) return getRandomColor() 
+}
+
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -45,3 +51,42 @@ slider.addEventListener('input', function() {
     }
 }
   }, false)
+
+  let onoff = 0;
+
+  function erase() {
+    if (onoff === 0){
+        onoff = 1;
+        document.querySelector(".erase").style.backgroundColor = "grey"
+        for (let i = 0; i < size; i++){
+            for (let j = 0; j < size; j++){
+                document.querySelector(".container").addEventListener("mouseover", function(event){
+                    event.target.style.backgroundColor = "white"
+                })
+            }
+        }
+    } else {
+        onoff = 0;
+        document.querySelector(".erase").style.backgroundColor = "white"
+        for (let i = 0; i < size; i++){
+            for (let j = 0; j < size; j++){
+                document.querySelector(".container").addEventListener("mouseover", function(event){
+                    event.target.style.backgroundColor = getRandomColor()
+                })
+            }
+        }
+    }
+}
+
+colorPicker.addEventListener("input", updateFirst, false);
+colorPicker.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
+
+function clear() {
+    
+}
